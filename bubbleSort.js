268f745adc1e22752aacfb04a689c71e428fb839
Felix <81396251+adyelson@users.noreply.github.com>
2023-08-21 22:48:32 -0300

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,31 +2,28 @@
 function bubbleSort(lista) {
 
     let contador = 0;
-    let teveTroca = false;
+    let trocou = false;
 
     for (let w = 0; w < lista.length - 1; w++) {
-        teveTroca = false;
+        trocou = false;
         for (let x = 0; x < lista.length - w - 1; x++) {
             if (lista[x] > lista[x + 1]) {
                 let temp = lista[x];
                 lista[x] = lista[x + 1]
                 lista[x + 1] = temp;
-                teveTroca = true;
+                trocou = true;
             }
-            contador++
+            contador++          
         }
-        if (!teveTroca) {
+        if (!trocou) {
             break;
         }
     }
-    return [lista, contador];
+    console.log(lista)
+    console.log(contador)
 }
 
 
 var listaDeNumeros = [999, 99, 9, 9999, 99999, 111, 11111, 23, 18, 71, 93, 201, 11, 3, 0, 1, 223, 2000, 3491, 981, 38, 77]
 
-let listaOrdenada = bubbleSort(listaDeNumeros)[0]
-let contagem = bubbleSort(listaDeNumeros)[1]
-
-console.log(listaOrdenada)
-console.log(contagem)
+bubbleSort(listaDeNumeros)
