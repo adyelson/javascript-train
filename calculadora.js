a939dc292aba198802a9123bee716644f5ff3110
adyelson <adyelson.loureiro@gmail.com>
2023-08-23 02:04:54 +0000

    tudao22ago

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+exports.somar = function(x,y){
+    return x+y;
+}
+
+exports.subtrair = function(x,y){
+    return x-y;
+}
+
+exports.multiplicar = function(x,y){
+    return x*y;
+}
+
+exports.dividir = function(x,y){
+    return x/y;
+}
